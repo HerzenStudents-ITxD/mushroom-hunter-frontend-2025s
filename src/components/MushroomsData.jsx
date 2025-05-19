745a3923948a71d40acb186a1a51adc50f1c5ce2
MakarSafronov <makarsafronov1@gmail.com>
2025-05-19 15:19:26 +0300

    исправление

--- src/components/MushroomsData.jsx
+++ src/components/MushroomsData.jsx
@@ -1,24 +1,24 @@
 export const mushroomsData=[
     {
         id:"mushroom-1",
-        modelPath:"/models/obj_f_1500_v_752.obj",
-        mtlPath:"/models/obj_f_1500_v_752.mtl",
+        modelPath:`${import.meta.env.BASE_URL}models/obj_f_1500_v_752.obj`,
+        mtlPath:`${import.meta.env.BASE_URL}models/obj_f_1500_v_752.mtl`,
         position:[10,4.35,2],
         info:{
             type:"Козляк",
             description:"съедобный гриб",
-            image:"/images/kozlac.jpg",
+            image:`${import.meta.env.BASE_URL}images/kozlac.jpg`,
         },
     },
     {
         id:"mushroom-2",
-        modelPath:"/models/obj_f_1500_v_752.obj",
-        mtlPath:"/models/obj_f_1500_v_752.mtl",
+        modelPath:`${import.meta.env.BASE_URL}models/obj_f_1500_v_752.obj`,
+        mtlPath:`${import.meta.env.BASE_URL}models/obj_f_1500_v_752.mtl`,
         position:[1,4.35,6],
         info:{
             type:"Поганка",
             description:"несъедобный гриб",
-            image:"/images/poganka.png",
+            image:`${import.meta.env.BASE_URL}images/poganka.png`,
         },
     }
 ]
